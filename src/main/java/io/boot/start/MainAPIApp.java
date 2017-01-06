cd84cc523ef925f7e8e2bec133756a3da0147dd8
pranav <pranav@pranav-PC>
2017-01-06 17:31:21 -0500

    first commit for spring boot application

---
+++ src/main/java/io/boot/start/MainAPIApp.java
@@ -0,0 +1,13 @@
+package io.boot.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainAPIApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainAPIApp.class, args);
+	}
+
+}
